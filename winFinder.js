134ddee860143d75327fb9eb8f2cf8f894ce0fc9
Jenny Bacon <jenniferbacon01@gmail.com>
2017-10-04 12:13:56 +0100

    refactor winfinder calculate win method out into 2 methods

--- winFinder.js
+++ winFinder.js
@@ -7,28 +7,30 @@
 		var that = this;
 		var outcome = 'no wins';
 		combinations.forEach(function(combination){
-			if(that.calculateWin(combination) === 'player1 wins'){
-				outcome = 'player1 wins';
-			}else if(that.calculateWin(combination) === 'player2 wins'){
-				outcome = 'player2 wins';
-			}
+      if(that.calculateWin(combination) != 'no wins'){
+        outcome = that.calculateWin(combination);
+      };
 		});
 		return outcome;
 	};
 
 	WinFinder.prototype.calculateWin = function(combination){
-		if (combination[0]===combination[1] && combination[1]===combination[2]){
-			if (combination[0]===1){
-				return 'player1 wins';
-			}else if (combination[0]===2) {
-				return 'player2 wins';
-			}else{
-				return 'no wins';
-			}
-		}else{
-			return 'no wins';
-		}
+    if (combination[0] != combination[1] || combination[1]!=combination[2]){
+      return 'no wins';
+    }else{
+      return this.calculateWinnerFor3Matching(combination);
+    };
 	};
+
+  WinFinder.prototype.calculateWinnerFor3Matching = function (combination) {
+    if (combination[0]===1){
+      return 'player1 wins';
+    }else if (combination[0]===2) {
+      return 'player2 wins';
+    }else{
+      return 'no wins';
+    }
+  };
 
 	exports.WinFinder = WinFinder;
 
